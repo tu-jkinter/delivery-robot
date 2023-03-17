4ab34308a9fc6e10880e17ac6c09d52f3daee9d9
Adam Love <adammlove01@gmail.com>
2023-03-16 21:54:24 -0400

    create accelerometer class

---
+++ accelerometer.cpp
@@ -0,0 +1,40 @@
+#include <Wire.h>
+#include "JY901.h"
+
+// serial baud rate must be 115200
+
+float xAcc(){
+  return (float)JY901.stcAcc.a[0]/32768*16;
+}
+
+float yAcc(){
+  return (float)JY901.stcAcc.a[1]/32768*16;
+}
+
+float zAcc(){
+  return (float)JY901.stcAcc.a[2]/32768*16;
+}
+
+float xGyro(){
+  return (float)JY901.stcGyro.w[0]/32768*2000;
+}
+
+float yGyro(){
+  return (float)JY901.stcGyro.w[1]/32768*2000;
+}
+
+float zGyro(){
+  return (float)JY901.stcGyro.w[2]/32768*2000;
+}
+
+float xAngle(){
+  return (float)JY901.stcAngle.Angle[0]/32768*180;
+}
+
+float yAngle(){
+  return (float)JY901.stcAngle.Angle[1]/32768*180;
+}
+
+float zAngle(){
+  return (float)JY901.stcAngle.Angle[2]/32768*180;
+}
---
+++ accelerometer.h
@@ -0,0 +1,17 @@
+float xAcc();
+
+float yAcc();
+
+float zAcc();
+
+float xGyro();
+
+float yGyro();
+
+float zGyro();
+
+float xAngle();
+
+float yAngle();
+
+float zAngle();
